4995b6f8d6db3d070b13ef6dfb94d43a439d74f6
dougiesquire <dougiesquire@gmail.com>
2021-06-21 11:05:01 +1000

    initial commit

---
+++ changes_in_namelists.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DIR=$1
+START_DATE=$2
+
+inputs=( `ls ${DIR}/MOM/input.nml.*-*` )
+n_inputs=${#inputs[@]}
+for i in $(seq 0 $(( $n_inputs - 2 ))); do
+	 DIFF=$(diff ${inputs[i]} ${inputs[i+1]})
+	 if [ "$DIFF" != "" ]; then
+		 LEAD=$(cut -d "-" -f2- <<< `basename ${inputs[i]}`)
+		 DATE=$(date -d "$START_DATE+$LEAD month" +%Y%m%d)
+		 echo "==================================================="
+		 echo "The following changes are made in $DATE:"
+		 echo "==================================================="
+		 echo -e "$DIFF"
+		 echo ""
+	 fi
+done
+
